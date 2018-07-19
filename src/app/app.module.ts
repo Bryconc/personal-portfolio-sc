54f22ee2afbd0ba15e17ebb2c2e7d369344baa45
Brycon Carpenter <bryconc@gmail.com>
2018-07-18 23:25:54 -0400

    Setup basic routing architecture. Add Reset SASS rules. Add Core and Feature module. Begin work on Home module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '../../node_modules/@angular/router';
+import { HomeModule } from './home/home.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeModule,
+    CoreModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

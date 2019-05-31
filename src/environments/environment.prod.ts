189927fccf8a5345751c2e8e911ad5a6ee28bb25
Brycon <bryconc@gmail.com>
2019-05-30 23:40:38 -0400

    Add tile background for feature panels.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: true,
   links: {
-    linkedIn: 'https://linkedin.com/in/bryconc'
+    linkedIn: 'https://linkedin.com/in/bryconc',
+    email: 'bryconc@gmail.com',
+    github: 'https://github.com/bryconc',
+    resume: 'https://drive.google.com/file/d/0B8gACi5Av5KxSk1BUGF5dWRGV1U/view?usp=sharing'
   }
 };

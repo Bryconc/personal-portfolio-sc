0a7ca765412fb041a723b4c3853da6731e728214
Brycon <bryconc@gmail.com>
2019-06-11 22:52:31 -0400

    Refactor how sass imports are defined.
    Finish terminal styling and data.

--- src/data/about/terminal-commands.js
+++ src/data/about/terminal-commands.js
@@ -3,5 +3,17 @@ export const commands = [
     command: 'BryconC.interests',
     output: '["Gaming", "Film", "Cars", "Programming"]'
   },
-  { command: 'BryconC.homeTown', output: 'Thomasville, NC' }
+  { command: 'BryconC.homeTown', output: 'Thomasville, NC' },
+  {
+    command: 'BryconC.education',
+    output: 'Appalachian State University, Boone, NC'
+  },
+  {
+    command: 'BryconC.degree',
+    output: 'Bachelor of Science in Computer Science'
+  },
+  {
+    command: 'BryconC.graduation',
+    output: 'May 2017'
+  }
 ];
